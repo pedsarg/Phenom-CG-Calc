8265bb41f4761f8e394a060f07aa82968e942b9b
Pedro Sartori Giorgetti <pedsar@gmail.com>
2025-03-12 12:38:32 -0300

    Graph Generator

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import math
-
+import matplotlib.pyplot as plt
 
 def getFuelValue(fuelWeight):
     try:
@@ -108,6 +108,37 @@ def calculateCGAndWeight(adjustedZeroFuel, fuelWeight, FuelMoment):
 def calculateCG(arm):
     return (((arm - 5.325) / 1.64) * 100)
 
+
+def graphGenerator(graphInformation):
+    limitWeight = [3000, 3220, 4030, 4750, 4750, 4030, 3420, 3000]
+    limitCG = [35.0, 21.5, 21.5, 23.5, 36.9, 38.5, 38.5, 35.0]
+
+    lineAboveWeight = [4430, 4430]
+    lineAboveCG = [22.7 , 37.6]
+
+    sideLineWeight = [3220, 3220, 4030, 4750, 4750]
+    sideLineCG = [21.5, 19.5, 19.5, 21.5, 23.5]
+
+    fig, ax = plt.subplots(figsize=(6, 8))
+
+    ax.plot(limitCG, limitWeight, 'k-')
+    ax.plot(lineAboveCG, lineAboveWeight, 'k-')
+    ax.plot(sideLineCG, sideLineWeight, 'k--')
+
+    ax.scatter(graphInformation['takeOffCG'], graphInformation['takeOffWeight'], color='red', zorder=3, label="Take Off")
+    ax.scatter(graphInformation['landingCG'], graphInformation['landingWeight'], color='blue', zorder=3, label="Landing")
+
+    ax.set_xlabel("CG POSITION - %MAC")
+    ax.set_ylabel("Weight (kg)")
+    ax.set_title("CG Position")
+    ax.legend()
+
+    ax.grid(True, linestyle="--", alpha=0.6)
+
+    plt.savefig("graph.png", dpi=300, bbox_inches='tight')
+
+
+
 def main():
     userInformation = getUserInput()
     weights = initializeWeightValues()
@@ -126,8 +157,17 @@ def main():
     cgAndWeightLanding = calculateCGAndWeight(adjustedZeroFuel, userInformation['landingFuelWeight'], landingFuelMoment)
     landingCG = calculateCG(cgAndWeightLanding['arm'])
     
-    print(f"CG: {takeOffCG:.3f}") 
-    print(f"Landing CG: {landingCG:.3f}")
+    graphInformation = {
+        'takeOffCG': takeOffCG,
+        'takeOffWeight': cgAndWeightTakeoff['weight'],
+        'landingCG': landingCG,
+        'landingWeight': cgAndWeightLanding['weight']
+    }
+
+    graphGenerator(graphInformation)
+
+    print(f"CG: {takeOffCG:.3f} \nWeight: {cgAndWeightTakeoff['weight']:.3f}") 
+    print(f"\nLanding CG: {landingCG:.3f} \nLanding Weight: {cgAndWeightLanding['weight']:.3f}")
 
 
 if __name__ == "__main__": 
